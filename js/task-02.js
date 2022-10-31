5c35d3c685b454b2424e5749e6a51017e948f637
npakypop <leaterood@gmail.com>
2022-10-31 21:30:00 +0200

    fix 2 task

--- js/task-02.js
+++ js/task-02.js
@@ -6,14 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const ingredientsRef = document.querySelector('#ingredients');
-console.log("ingredientsRef", ingredientsRef);
 
-const ingredientsList = ingredients.forEach(el => {
+const ingredientsRef = document.querySelector('#ingredients');
+
+const ingredientsList = [];
+
+ingredients.forEach(el => {
   const itemRef = document.createElement('li');
   itemRef.textContent = el;
   itemRef.classList.add('item');
-  ingredientsRef.append(itemRef);
+  ingredientsList.push(itemRef);
 });
+
+ingredientsRef.append(...ingredientsList);
+console.log("ingredientsRef", ingredientsRef);
+
+
 
 
