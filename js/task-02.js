280772899674d28ed79ca51ea844ef5726defe94
npakypop <leaterood@gmail.com>
2022-10-31 20:57:27 +0200

    2 task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsRef = document.querySelector('#ingredients');
+console.log("ingredientsRef", ingredientsRef);
+
+const ingredientsList = ingredients.forEach(el => {
+  const itemRef = document.createElement('li');
+  itemRef.textContent = el;
+  itemRef.classList.add('item');
+  ingredientsRef.append(itemRef);
+});
+
+
