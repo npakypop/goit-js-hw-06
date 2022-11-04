194bed3c65d5813e005633b87b6d7c1be1be2543
npakypop <leaterood@gmail.com>
2022-11-04 17:25:26 +0200

    fix 1

--- js/task-01.js
+++ js/task-01.js
@@ -7,12 +7,18 @@ console.log("Number of categories: ", number);
 const itemRef = [...categoriesRef.children];
 // console.log("itemRef", itemRef);
 
-const itemNumber = itemRef.map(el => { 
+itemRef.forEach(el => { 
     const titleRef = el.firstElementChild;
     const elements = el.lastElementChild.children.length;
     console.log("Category: ", titleRef.textContent);
     console.log("Elements: ", elements);
 });
+// const itemNumber = itemRef.map(el => { 
+//     const titleRef = el.firstElementChild;
+//     const elements = el.lastElementChild.children.length;
+//     console.log("Category: ", titleRef.textContent);
+//     console.log("Elements: ", elements);
+// });
 
 
 
